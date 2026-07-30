5e264a51495305e4d579c2ab57713b46b4b9784b
Yagmur <yagmureser10@gmail.com>
2026-07-30 13:49:31 +0300

    Complete DR-1001 admin user creation

---
+++ app/Http/Controllers/AdminUserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AdminUserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => 'required|string|max:255|alpha_num|unique:users,username',
+            'user_title' => 'required|string|max:255',
+            'password' => 'required|string|min:6',
+        ]);
+
+        User::create([
+            'username' => $validated['username'],
+            'user_title' => $validated['user_title'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return redirect()
+            ->route('admin.users.index')
+            ->with('success', 'Kullanıcı başarıyla eklendi.');
+    }
+}
